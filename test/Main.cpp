17c46ef4f92398234e525185042d8f3cd792e70d
Thresh <1060037850@qq.com>
2024-03-06 11:10:39 +0000

    Revert "删除文件 test"
    This reverts commit 62dabfe82979222fc437e1c3415aeb6a553d7161.

---
+++ test/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Solutions.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+
+
+
+	return 0;
+}
---
+++ test/Solutions.h
@@ -0,0 +1,42 @@
+#ifndef __SOLUTIONS_H__
+#define __SOLUTIONS_H__
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+/*  2.两数相加
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() :val(0), next(nullptr) {}
+	ListNode(int x) :val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) :val(x), next(next) {}
+};
+*/
+
+class Solutions
+{
+public:
+	Solutions();
+	~Solutions();
+
+	//std::vector<int> TwoSum(vector<int>& nums, int target);          //1.两数之和
+
+	//ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);          //2.两数相加
+
+	//int lengthOfLongestSubstring(std::string s);          //3.无重复字符的最长子串
+
+	//int getKthElement(const std::vector<int>& nums1, const std::vector<int>& nums2, int k);
+	//double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2);          //4. 寻找两个正序数组的中位数
+
+	//std::string longestPalindrome(std::string s);          //5.最长回文子串
+
+	//std::string convert(std::string s, int numRows);		  //6.Z字形变换
+private:
+
+};
+
+
+#endif //! __SOLUTIONS_H__
